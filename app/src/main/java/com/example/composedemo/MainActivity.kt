06fb803fb7562266e203b05884b9bf0ee11136ee
abdul <abdul.tanveer@gmail.com>
2024-09-17 10:15:35 +0530

    state

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,11 +54,16 @@ fun MyApp(modifier: Modifier = Modifier,
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded = remember {mutableStateOf(false)  }// Don't do this!
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(color = MaterialTheme.colorScheme.primary,
             modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
 
         Row(modifier = Modifier.padding(24.dp)) {
-            Column {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(
                     text = "Hello"
                 )
@@ -58,17 +72,42 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 )
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
 
         }
 
     }
 }
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
 
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("Continue")
+        }
+    }
+}
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeDemoTheme {
+        OnboardingScreen()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
